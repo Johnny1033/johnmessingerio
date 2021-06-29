de3a274661b17b1ec408b66e8329da3fcf06c5de
John M <Johnny1033@gmail.com>
2021-06-29 13:40:26 -0400

    removed icon and updated link color

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src='https://styles.redditmedia.com/t5_2th52/styles/communityIcon_b37n2zfs8k861.png?width=256&s=a9cb63319055ded916e5b12ff03f1e8c63ae6911'></img>
         <p>
           Hello.
         </p>
