64efef0b811b8a4f1e7d9e7eb16e303c3bf59897
John M <Johnny1033@gmail.com>
2021-06-29 13:06:56 -0400

    initial push

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src='https://styles.redditmedia.com/t5_2th52/styles/communityIcon_b37n2zfs8k861.png?width=256&s=a9cb63319055ded916e5b12ff03f1e8c63ae6911'></img>
+        <p>
+          Hello.
+        </p>
+        <a
+          className="App-link"
+          href="https://www.linkedin.com/in/johnamessinger/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          This is not a phishing link
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
